d29c2c2dab22d6e6296e647895e89cdd905d1330
SandraGdd <167540971+SandraGdd@users.noreply.github.com>
2024-10-18 17:48:15 +0200

    tenure

--- functionst.py
+++ functionst.py
@@ -4,17 +4,27 @@
 def age_group(x):
     if x < 24:
         return "teens"
-    elif x >= 24 and x < 34:
+    elif x < 34:
         return "young adults"
-    elif x >= 34 and x < 44:
+    elif x < 44:
         return "adults"
-    elif x >= 44 and x < 54:
+    elif x < 54:
         return "older adults"
-    elif x >= 54 and x < 64:
+    elif x < 64:
         return "old adults"
-    elif x >= 64 and x < 74:
+    elif x < 74:
         return "young senior"
-    elif x >= 74 and x < 84:
+    elif x < 84:
         return "senior"
     else:
         return "old senior"
+
+
+# Define client tenure
+
+def client_tenure(x):
+    if x < 11:
+        return "short term user"
+    else:
+        return "long term user"
+
