1a14c4665e7cc946c77a697212ac433a5b978278
BigKawa <164774630+BigKawa@users.noreply.github.com>
2024-10-20 17:03:43 +0200

    Updated Digital Footpring (Finished visits)
    # Added a function to calculate the finshed visits which went trough all steps
    
    Added the column the DigitalFootprint

--- functionst.py
+++ functionst.py
@@ -9,3 +9,36 @@ def age_group(x):
         return "adult"
     else:
         return "senior"
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+def filter_complete_visit(df):
+    """This functions checks if the visit ids have gone through 
+    all the required process steps to count as a confirmed state"""
+    all_steps = ['step_3', 'step_2', 'step_1', 'start', 'confirm']
+
+    
+    visit_check = df.groupby("visit_id")["process_step"].apply(lambda x:all((step in x.values for step in all_steps)))
+    return visit_check[visit_check == True].index.tolist()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
